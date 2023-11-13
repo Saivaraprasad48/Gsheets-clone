506b5510eadfefc1997834e5d572203a45295051
Saivaraprasad48 <sai44prasad88@gmail.com>
2023-11-13 15:24:09 +0530

    updated complete code

--- cell-properties.js
+++ cell-properties.js
@@ -1,10 +1,10 @@
 // Storage
-let collectedSheetDB = [];  //Contains all SheetDB
+let collectedSheetDB = []; //Contains all SheetDB
 let sheetDB = [];
 
 {
-    let addSheetBtn = document.querySelector(".sheet-add-icon");
-    addSheetBtn.click();
+  let addSheetBtn = document.querySelector(".sheet-add-icon");
+  addSheetBtn.click();
 }
 
 // for (let i = 0; i < rows; i++) {
@@ -28,7 +28,6 @@ let sheetDB = [];
 //     sheetDB.push(sheetRow);
 // }
 
-
 // Selectors for cell properties
 let bold = document.querySelector(".bold");
 let italic = document.querySelector(".italic");
@@ -48,162 +47,175 @@ let inactiveColorProp = "#ecf0f1";
 // Application of two-way binding
 // Attach property listeners
 bold.addEventListener("click", (e) => {
-    let address = addressBar.value;
-    let [cell, cellProp] = getCellAndCellProp(address);
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
 
-    // Modification
-    cellProp.bold = !cellProp.bold; // Data change
-    cell.style.fontWeight = cellProp.bold ? "bold" : "normal"; // UI change (1)
-    bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactiveColorProp; // UI change (2)
-})
+  // Modification
+  cellProp.bold = !cellProp.bold; // Data change
+  cell.style.fontWeight = cellProp.bold ? "bold" : "normal"; // UI change (1)
+  bold.style.backgroundColor = cellProp.bold
+    ? activeColorProp
+    : inactiveColorProp; // UI change (2)
+});
 italic.addEventListener("click", (e) => {
-    let address = addressBar.value;
-    let [cell, cellProp] = getCellAndCellProp(address);
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
 
-    // Modification
-    cellProp.italic = !cellProp.italic; // Data change
-    cell.style.fontStyle = cellProp.italic ? "italic" : "normal"; // UI change (1)
-    italic.style.backgroundColor = cellProp.italic ? activeColorProp : inactiveColorProp; // UI change (2)
-})
+  // Modification
+  cellProp.italic = !cellProp.italic; // Data change
+  cell.style.fontStyle = cellProp.italic ? "italic" : "normal"; // UI change (1)
+  italic.style.backgroundColor = cellProp.italic
+    ? activeColorProp
+    : inactiveColorProp; // UI change (2)
+});
 underline.addEventListener("click", (e) => {
-    let address = addressBar.value;
-    let [cell, cellProp] = getCellAndCellProp(address);
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
 
-    // Modification
-    cellProp.underline = !cellProp.underline; // Data change
-    cell.style.textDecoration = cellProp.underline ? "underline" : "none"; // UI change (1)
-    underline.style.backgroundColor = cellProp.underline ? activeColorProp : inactiveColorProp; // UI change (2)
-})
+  // Modification
+  cellProp.underline = !cellProp.underline; // Data change
+  cell.style.textDecoration = cellProp.underline ? "underline" : "none"; // UI change (1)
+  underline.style.backgroundColor = cellProp.underline
+    ? activeColorProp
+    : inactiveColorProp; // UI change (2)
+});
 fontSize.addEventListener("change", (e) => {
-    let address = addressBar.value;
-    let [cell, cellProp] = getCellAndCellProp(address);
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
 
-    cellProp.fontSize = fontSize.value; // Data change
-    cell.style.fontSize = cellProp.fontSize + "px";
-    fontSize.value = cellProp.fontSize;
-})
+  cellProp.fontSize = fontSize.value; // Data change
+  cell.style.fontSize = cellProp.fontSize + "px";
+  fontSize.value = cellProp.fontSize;
+});
 fontFamily.addEventListener("change", (e) => {
-    let address = addressBar.value;
-    let [cell, cellProp] = getCellAndCellProp(address);
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
 
-    cellProp.fontFamily = fontFamily.value; // Data change
-    cell.style.fontFamily = cellProp.fontFamily;
-    fontFamily.value = cellProp.fontFamily;
-})
+  cellProp.fontFamily = fontFamily.value; // Data change
+  cell.style.fontFamily = cellProp.fontFamily;
+  fontFamily.value = cellProp.fontFamily;
+});
 fontColor.addEventListener("change", (e) => {
-    let address = addressBar.value;
-    let [cell, cellProp] = getCellAndCellProp(address);
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
 
-    cellProp.fontColor = fontColor.value; // Data change
-    cell.style.color = cellProp.fontColor;
-    fontColor.value = cellProp.fontColor;
-})
+  cellProp.fontColor = fontColor.value; // Data change
+  cell.style.color = cellProp.fontColor;
+  fontColor.value = cellProp.fontColor;
+});
 BGcolor.addEventListener("change", (e) => {
+  let address = addressBar.value;
+  let [cell, cellProp] = getCellAndCellProp(address);
+
+  cellProp.BGcolor = BGcolor.value; // Data change
+  cell.style.backgroundColor = cellProp.BGcolor;
+  BGcolor.value = cellProp.BGcolor;
+});
+alignment.forEach((alignElem) => {
+  alignElem.addEventListener("click", (e) => {
     let address = addressBar.value;
     let [cell, cellProp] = getCellAndCellProp(address);
 
-    cellProp.BGcolor = BGcolor.value; // Data change
-    cell.style.backgroundColor = cellProp.BGcolor;
-    BGcolor.value = cellProp.BGcolor;
-})
-alignment.forEach((alignElem) => {
-    alignElem.addEventListener("click", (e) => {
-        let address = addressBar.value;
-        let [cell, cellProp] = getCellAndCellProp(address);
+    let alignValue = e.target.classList[0];
+    cellProp.alignment = alignValue; // Data change
+    cell.style.textAlign = cellProp.alignment; // UI change (1)
 
-        let alignValue = e.target.classList[0];
-        cellProp.alignment = alignValue; // Data change
-        cell.style.textAlign = cellProp.alignment; // UI change (1)
-
-        switch(alignValue) { // UI change (2)
-            case "left":
-                leftAlign.style.backgroundColor = activeColorProp;
-                centerAlign.style.backgroundColor = inactiveColorProp;
-                rightAlign.style.backgroundColor = inactiveColorProp;
-                break;
-            case "center":
-                leftAlign.style.backgroundColor = inactiveColorProp;
-                centerAlign.style.backgroundColor = activeColorProp;
-                rightAlign.style.backgroundColor = inactiveColorProp;
-                break;
-            case "right":
-                leftAlign.style.backgroundColor = inactiveColorProp;
-                centerAlign.style.backgroundColor = inactiveColorProp;
-                rightAlign.style.backgroundColor = activeColorProp;
-                break;
-        }
-
-    })
-})
-
-
+    switch (
+      alignValue // UI change (2)
+    ) {
+      case "left":
+        leftAlign.style.backgroundColor = activeColorProp;
+        centerAlign.style.backgroundColor = inactiveColorProp;
+        rightAlign.style.backgroundColor = inactiveColorProp;
+        break;
+      case "center":
+        leftAlign.style.backgroundColor = inactiveColorProp;
+        centerAlign.style.backgroundColor = activeColorProp;
+        rightAlign.style.backgroundColor = inactiveColorProp;
+        break;
+      case "right":
+        leftAlign.style.backgroundColor = inactiveColorProp;
+        centerAlign.style.backgroundColor = inactiveColorProp;
+        rightAlign.style.backgroundColor = activeColorProp;
+        break;
+    }
+  });
+});
 
 let allCells = document.querySelectorAll(".cell");
-for (let i = 0;i < allCells.length;i++) {
-    addListenerToAttachCellProperties(allCells[i]);
+for (let i = 0; i < allCells.length; i++) {
+  addListenerToAttachCellProperties(allCells[i]);
 }
 
 function addListenerToAttachCellProperties(cell) {
-    // Work
-    cell.addEventListener("click", (e) => {
-        let address = addressBar.value;
-        let [rid, cid] = decodeRIDCIDFromAddress(address);
-        let cellProp = sheetDB[rid][cid];
+  // Work
+  cell.addEventListener("click", (e) => {
+    let address = addressBar.value;
+    let [rid, cid] = decodeRIDCIDFromAddress(address);
+    let cellProp = sheetDB[rid][cid];
 
-        // Apply cell Properties
-        cell.style.fontWeight = cellProp.bold ? "bold" : "normal";
-        cell.style.fontStyle = cellProp.italic ? "italic" : "normal";
-        cell.style.textDecoration = cellProp.underline ? "underline" : "none";
-        cell.style.fontSize = cellProp.fontSize + "px";
-        cell.style.fontFamily = cellProp.fontFamily;
-        cell.style.color = cellProp.fontColor;
-        cell.style.backgroundColor = cellProp.BGcolor === "#000000" ? "transparent" : cellProp.BGcolor;
-        cell.style.textAlign = cellProp.alignment;
-                
+    // Apply cell Properties
+    cell.style.fontWeight = cellProp.bold ? "bold" : "normal";
+    cell.style.fontStyle = cellProp.italic ? "italic" : "normal";
+    cell.style.textDecoration = cellProp.underline ? "underline" : "none";
+    cell.style.fontSize = cellProp.fontSize + "px";
+    cell.style.fontFamily = cellProp.fontFamily;
+    cell.style.color = cellProp.fontColor;
+    cell.style.backgroundColor =
+      cellProp.BGcolor === "#000000" ? "transparent" : cellProp.BGcolor;
+    cell.style.textAlign = cellProp.alignment;
 
-        // Apply properties UI Props container
-        bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactiveColorProp;
-        italic.style.backgroundColor = cellProp.italic ? activeColorProp : inactiveColorProp;
-        underline.style.backgroundColor = cellProp.underline ? activeColorProp : inactiveColorProp;
-        fontColor.value = cellProp.fontColor;
-        BGcolor.value = cellProp.BGcolor;
-        fontSize.value = cellProp.fontSize;
-        fontFamily.value = cellProp.fontFamily;
-        switch(cellProp.alignment) { // UI change (2)
-            case "left":
-                leftAlign.style.backgroundColor = activeColorProp;
-                centerAlign.style.backgroundColor = inactiveColorProp;
-                rightAlign.style.backgroundColor = inactiveColorProp;
-                break;
-            case "center":
-                leftAlign.style.backgroundColor = inactiveColorProp;
-                centerAlign.style.backgroundColor = activeColorProp;
-                rightAlign.style.backgroundColor = inactiveColorProp;
-                break;
-            case "right":
-                leftAlign.style.backgroundColor = inactiveColorProp;
-                centerAlign.style.backgroundColor = inactiveColorProp;
-                rightAlign.style.backgroundColor = activeColorProp;
-                break;
-        }
+    // Apply properties UI Props container
+    bold.style.backgroundColor = cellProp.bold
+      ? activeColorProp
+      : inactiveColorProp;
+    italic.style.backgroundColor = cellProp.italic
+      ? activeColorProp
+      : inactiveColorProp;
+    underline.style.backgroundColor = cellProp.underline
+      ? activeColorProp
+      : inactiveColorProp;
+    fontColor.value = cellProp.fontColor;
+    BGcolor.value = cellProp.BGcolor;
+    fontSize.value = cellProp.fontSize;
+    fontFamily.value = cellProp.fontFamily;
+    switch (
+      cellProp.alignment // UI change (2)
+    ) {
+      case "left":
+        leftAlign.style.backgroundColor = activeColorProp;
+        centerAlign.style.backgroundColor = inactiveColorProp;
+        rightAlign.style.backgroundColor = inactiveColorProp;
+        break;
+      case "center":
+        leftAlign.style.backgroundColor = inactiveColorProp;
+        centerAlign.style.backgroundColor = activeColorProp;
+        rightAlign.style.backgroundColor = inactiveColorProp;
+        break;
+      case "right":
+        leftAlign.style.backgroundColor = inactiveColorProp;
+        centerAlign.style.backgroundColor = inactiveColorProp;
+        rightAlign.style.backgroundColor = activeColorProp;
+        break;
+    }
 
-        let formulaBar = document.querySelector(".formula-bar");
-        formulaBar.value = cellProp.formula;
-        cell.innerText = cellProp.value;
-    })
+    let formulaBar = document.querySelector(".formula-bar");
+    formulaBar.value = cellProp.formula;
+    cell.innerText = cellProp.value;
+  });
 }
 
 function getCellAndCellProp(address) {
-    let [rid, cid] = decodeRIDCIDFromAddress(address);
-    // Access cell & storage object
-    let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
-    let cellProp = sheetDB[rid][cid];
-    return [cell, cellProp];
+  let [rid, cid] = decodeRIDCIDFromAddress(address);
+  // Access cell & storage object
+  let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+  let cellProp = sheetDB[rid][cid];
+  return [cell, cellProp];
 }
 
 function decodeRIDCIDFromAddress(address) {
-    // address -> "A1"
-    let rid = Number(address.slice(1) - 1); // "1" -> 0
-    let cid = Number(address.charCodeAt(0)) - 65; // "A" -> 65
-    return [rid, cid];
+  // address -> "A1"
+  let rid = Number(address.slice(1) - 1); // "1" -> 0
+  let cid = Number(address.charCodeAt(0)) - 65; // "A" -> 65
+  return [rid, cid];
 }
--- cycleValidation.js
+++ cycleValidation.js
@@ -13,32 +13,38 @@ let graphComponentMatrix = [];
 
 // True -> cyclic, False -> Not cyclic
 function isGraphCylic(graphComponentMatrix) {
-    // Dependency -> visited, dfsVisited (2D array)
-    let visited = []; // Node visit trace
-    let dfsVisited = []; // Stack visit trace
+  // Dependency -> visited, dfsVisited (2D array)
+  let visited = []; // Node visit trace
+  let dfsVisited = []; // Stack visit trace
 
-    for (let i = 0; i < rows; i++) {
-        let visitedRow = [];
-        let dfsVisitedRow = [];
-        for (let j = 0; j < cols; j++) {
-            visitedRow.push(false);
-            dfsVisitedRow.push(false);
-        }
-        visited.push(visitedRow);
-        dfsVisited.push(dfsVisitedRow);
+  for (let i = 0; i < rows; i++) {
+    let visitedRow = [];
+    let dfsVisitedRow = [];
+    for (let j = 0; j < cols; j++) {
+      visitedRow.push(false);
+      dfsVisitedRow.push(false);
     }
+    visited.push(visitedRow);
+    dfsVisited.push(dfsVisitedRow);
+  }
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            if (visited[i][j] === false) {
-                let response = dfsCycleDetection(graphComponentMatrix, i, j, visited, dfsVisited);
-                // Found cycle so return immediately, no need to explore more path
-                if (response == true) return [i, j];
-            }
-        }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (visited[i][j] === false) {
+        let response = dfsCycleDetection(
+          graphComponentMatrix,
+          i,
+          j,
+          visited,
+          dfsVisited
+        );
+        // Found cycle so return immediately, no need to explore more path
+        if (response == true) return [i, j];
+      }
     }
-    
-    return null;
+  }
+
+  return null;
 }
 
 // Start -> vis(TRUE) dfsVis(TRUE)
@@ -47,23 +53,41 @@ function isGraphCylic(graphComponentMatrix) {
 // Cycle detection condition -> if (vis[i][j] == true && dfsVis[i][j] == true) -> cycle
 // Return -> True/False
 // True -> cyclic, False -> Not cyclic
-function dfsCycleDetection(graphComponentMatrix, srcr, srcc, visited, dfsVisited) {
-    visited[srcr][srcc] = true;
-    dfsVisited[srcr][srcc] = true;
+function dfsCycleDetection(
+  graphComponentMatrix,
+  srcr,
+  srcc,
+  visited,
+  dfsVisited
+) {
+  visited[srcr][srcc] = true;
+  dfsVisited[srcr][srcc] = true;
 
-    // A1 -> [ [0, 1], [1, 0], [5, 10], .....  ]
-    for (let children = 0; children < graphComponentMatrix[srcr][srcc].length; children++) {
-        let [nbrr, nbrc] = graphComponentMatrix[srcr][srcc][children];
-        if (visited[nbrr][nbrc] === false) {
-            let response = dfsCycleDetection(graphComponentMatrix, nbrr, nbrc, visited, dfsVisited);
-            if (response === true) return true; // Found cycle so return immediately, no need to explore more path
-        }
-        else if (visited[nbrr][nbrc] === true && dfsVisited[nbrr][nbrc] === true) {
-            // Found cycle so return immediately, no need to explore more path
-            return true;
-        }
+  // A1 -> [ [0, 1], [1, 0], [5, 10], .....  ]
+  for (
+    let children = 0;
+    children < graphComponentMatrix[srcr][srcc].length;
+    children++
+  ) {
+    let [nbrr, nbrc] = graphComponentMatrix[srcr][srcc][children];
+    if (visited[nbrr][nbrc] === false) {
+      let response = dfsCycleDetection(
+        graphComponentMatrix,
+        nbrr,
+        nbrc,
+        visited,
+        dfsVisited
+      );
+      if (response === true) return true; // Found cycle so return immediately, no need to explore more path
+    } else if (
+      visited[nbrr][nbrc] === true &&
+      dfsVisited[nbrr][nbrc] === true
+    ) {
+      // Found cycle so return immediately, no need to explore more path
+      return true;
     }
+  }
 
-    dfsVisited[srcr][srcc] = false;
-    return false;
+  dfsVisited[srcr][srcc] = false;
+  return false;
 }
